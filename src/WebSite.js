33baafc537073cff48156055c790c1ae8660b531
lijian <1376623234@qq.com>
2021-09-07 17:24:52 +0800

    1.一个js文件里面可以写多个类 2.引入组件可以是同一个js中引入，也可以是不同js中引入

---
+++ src/WebSite.js
@@ -0,0 +1,41 @@
+import React from "react";
+// import Name from "./Name";
+// import Link from "./Link";
+class WebSite extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "菜鸟教程",
+      site: "https://www.runoob.com"
+    }
+  }
+  render() {
+    return (
+        <div>
+          <Name name={this.state.name}/>
+          <Link site={this.state.site}/>
+        </div>
+    );
+  }
+}
+
+class Name extends React.Component {
+    render() {
+        return (
+            <div>
+                {this.props.name}
+            </div>
+        );
+    }
+}
+
+
+class Link extends React.Component {
+    render(){
+        return (
+            <a href={this.props.site}>{this.props.site}</a>
+        )
+    }
+}
+export default WebSite;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 // import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Clock from './Clock'
+// import Clock from './Clock'
 // import Clockf from './Clockf'
+import WebSite from "./WebSite";
 ReactDOM.render(
-    <Clock/>,
+    <WebSite/>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
